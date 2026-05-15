750290d911501007a99280a6ce2b40598c161600
Lucas Trindade Da Silva <Lucas500000000@gmail.com>
2026-05-15 00:11:33 -0300

    Commit inicial: Setup inicial do projeto stackItem para gerenciamento de estoque

---
+++ src/main/java/com/lucas/stackitem/StackitemApplication.java
@@ -0,0 +1,14 @@
+package com.lucas.stackitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackitemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackitemApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/lucas/stackitem/StackitemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.stackitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackitemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
